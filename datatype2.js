8503362f399f22eade235de275f6240e2df89575
jarvinbai <jarvinbaij@gmail.com>
2021-07-20 03:26:54 +0530

    java added

---
+++ datatype2.js
@@ -0,0 +1,17 @@
+let a=25;
+let b=true;
+let c="ram";
+let d=25.56;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+var user={
+    "name":"Ram",
+    "age":25,
+    "isActive":true
+}
+console.log(user.name);
+console.log(user.age);
+console.log(user.isActive);
---
+++ hello.js
@@ -0,0 +1,5 @@
+console.log("welcome to node js");
+let a=25;
+let b=35;
+let c=a+b;
+console.log("total:"+c);
